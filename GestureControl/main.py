42ecafcb804d3aff4a696c46c55fe0123cb137bc
Naman-Adhikari <adhikarinaman00@gmail.com>
2026-03-16 21:59:32 +0545

    gesture workspace swipe

--- GestureControl/main.py
+++ GestureControl/main.py
@@ -1,9 +1,79 @@
 import cv2
 import time
+import os
 import subprocess
 import mediapipe as mp
 from mediapipe.tasks.python import vision
 from mediapipe.tasks import python
+
+
+last_switch = 0
+prev_x = None
+swipe_cooldown = 0
+
+def detect_swipe(hand_landmarks):
+    global prev_x, swipe_cooldown
+
+    if time.time() - swipe_cooldown < 1:
+        return
+
+    wrist = hand_landmarks[0]
+    current_x = wrist.x
+
+    if prev_x is None:
+        prev_x = current_x
+        return
+
+    dx = current_x - prev_x
+
+    threshold = 0.12   # adjust if needed
+
+    if dx > threshold:
+        subprocess.run(["hyprctl", "dispatch", "movefocus", "r"], env=os.environ)
+        swipe_cooldown = time.time()
+        prev_x = current_x
+        return
+
+    elif dx < -threshold:
+        subprocess.run(["hyprctl", "dispatch", "movefocus", "l"], env=os.environ)
+        swipe_cooldown = time.time()
+        prev_x = current_x
+        return
+
+    prev_x = current_x
+
+def switch_workspace_by_fingers(hand_landmarks):
+    global last_switch
+
+    if time.time() - last_switch < 1:
+        return
+
+    # landmark indices
+    tips = [4, 8, 12, 16, 20]
+    pips = [3, 6, 10, 14, 18]
+
+    fingers = []
+
+    # thumb
+    fingers.append(hand_landmarks[tips[0]].x > hand_landmarks[pips[0]].x)
+
+    # other fingers
+    for i in range(1, 5):
+        fingers.append(hand_landmarks[tips[i]].y < hand_landmarks[pips[i]].y)
+
+    count = sum(fingers)
+
+    if count == 1:
+        subprocess.run(["hyprctl", "dispatch", "workspace", "1"],env=os.environ)
+        last_switch = time.time()
+
+    elif count == 2:
+        subprocess.run(["hyprctl", "dispatch", "workspace", "2"],env=os.environ)
+        last_switch = time.time()
+
+    elif count == 3:
+        subprocess.run(["hyprctl", "dispatch", "workspace", "3"], env=os.environ)
+        last_switch = time.time()
 
 
 def get_fingers_up(hand):
@@ -49,36 +119,44 @@ while cap.isOpened():
     detection_result = hand_landmarker.detect(mp_image)
 
     if detection_result.hand_landmarks:
+        for i, hand in enumerate(detection_result.hand_landmarks):
+            handedness = detection_result.handedness[i][0].category_name
 
-        h, w, _ = frame.shape
+            if handedness == "Left":
+                switch_workspace_by_fingers(hand)
+                detect_swipe(hand)
 
-        for hand_landmarks in detection_result.hand_landmarks:
+    #if detection_result.hand_landmarks:
+#
+        #h, w, _ = frame.shape
+#
+        #for hand_landmarks in detection_result.hand_landmarks:
+#
+            #fingers = get_fingers_up(hand_landmarks)
+#
+            #gesture = None
+#
+            #if fingers == [True]:
+                #gesture = "THUMBS_UP"
+            #else:
+                #gesture = "THUMBS_DOWN"
+#
+            #if gesture == "THUMBS_UP" and previous_gesture != "THUMBS_UP":
+                #print("👍 Thumbs up detected!")
+                #subprocess.run(["/home/lostfromlight/.dotfiles/home/scripts/lightmode.sh"])
+            #elif gesture == "THUMBS_DOWN" and previous_gesture != "THUMBS_DOWN":
+                #subprocess.run(["/home/lostfromlight/.dotfiles/home/scripts/darkmode.sh"])
+#
+#
+            #previous_gesture = gesture
+#
+            #for landmark in hand_landmarks:
+                #x = int(landmark.x * w)
+                #y = int(landmark.y * h)
+#
+#                cv2.circle(frame, (x, y), 5, (0,255,0), -1)
 
-            fingers = get_fingers_up(hand_landmarks)
-
-            gesture = None
-
-            if fingers == [True]:
-                gesture = "THUMBS_UP"
-            else:
-                gesture = "THUMBS_DOWN"
-
-            if gesture == "THUMBS_UP" and previous_gesture != "THUMBS_UP":
-                print("👍 Thumbs up detected!")
-                subprocess.run(["/home/lostfromlight/.dotfiles/home/scripts/lightmode.sh"])
-            elif gesture == "THUMBS_DOWN" and previous_gesture != "THUMBS_DOWN":
-                subprocess.run(["/home/lostfromlight/.dotfiles/home/scripts/darkmode.sh"])
-
-
-            previous_gesture = gesture
-
-            for landmark in hand_landmarks:
-                x = int(landmark.x * w)
-                y = int(landmark.y * h)
-
-                cv2.circle(frame, (x, y), 5, (0,255,0), -1)
-
-    #cv2.imshow("Feed", frame)
+#    cv2.imshow("Feed", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
